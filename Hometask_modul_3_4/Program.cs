733c6c69312918f6a0bcd5942ceb16205bc40c73
Ravshan04 <rn3925471@gmail.com>
2025-07-03 16:51:52 +0500

    i have completed

--- Hometask_modul_3_4/Program.cs
+++ Hometask_modul_3_4/Program.cs
@@ -1,38 +1,69 @@
-﻿namespace Hometask_modul_3_4
+﻿using System;
+using System.Threading;
+
+class Program
 {
-    public class Program
+    static readonly object consoleLock = new object();
+
+    static AutoResetEvent thread1Turn = new AutoResetEvent(true);
+    static AutoResetEvent thread2Turn = new AutoResetEvent(false);
+    static AutoResetEvent thread3Turn = new AutoResetEvent(false);
+
+    static int maxSteps = 20;
+    static int[] steps = { 0, 0, 0 };
+
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Thread thread1 = new Thread(Thread1);
-            Thread thread2 = new Thread(Thread2);
-            thread1.Start();
-            thread2.Start();
-            ////wefwefwerferf
-            ///dfkwedfwe
-            ///edew
-            ///wedwed
-            ///
+        Console.Clear();
 
+        // Har bir threadga progress chizig‘i boshlab beriladi
+        lock (consoleLock)
+        {
+            Console.SetCursorPosition(0, 0); Console.Write("Thread-1: ");
+            Console.SetCursorPosition(0, 1); Console.Write("Thread-2: ");
+            Console.SetCursorPosition(0, 2); Console.Write("Thread-3: ");
         }
 
-        static void Thread1()
-        {
-            for (int i = 1; i <= 10; i++)
-            {
-                Console.Write("=");
-                Thread.Sleep(100);
-            }
-        }
+        // Threadlarni ishga tushuramiz
+        Thread t1 = new Thread(() => DoProgress(0, thread1Turn, thread2Turn, ConsoleColor.Cyan));
+        Thread t2 = new Thread(() => DoProgress(1, thread2Turn, thread3Turn, ConsoleColor.Yellow));
+        Thread t3 = new Thread(() => DoProgress(2, thread3Turn, thread1Turn, ConsoleColor.Green));
 
-        static void Thread2()
+        t1.Start();
+        t2.Start();
+        t3.Start();
+
+        t1.Join();
+        t2.Join();
+        t3.Join();
+
+        // Yakuniy xabar
+        lock (consoleLock)
         {
-            for (int i = 10; i >= 1; i--)
+            Console.SetCursorPosition(0, 4);
+            Console.ResetColor();
+            Console.WriteLine("✅ Barcha threadlar progressni tugatdi.");
+        }
+    }
+
+    static void DoProgress(int line, AutoResetEvent myTurn, AutoResetEvent nextTurn, ConsoleColor color)
+    {
+        while (steps[line] < maxSteps)
+        {
+            myTurn.WaitOne(); // navbat kelgunga qadar kutadi
+
+            steps[line]++;
+
+            lock (consoleLock)
             {
-                Console.Write("=");
-                Thread.Sleep(100);
+                Console.ForegroundColor = color;
+                Console.SetCursorPosition(10, line); // 10-x dan progress boshlab yoziladi
+                Console.Write(new string('=', steps[line]) + ">");
+                Console.ResetColor();
             }
-            Console.WriteLine();
+
+            Thread.Sleep(500); // progress sekinlashsin
+            nextTurn.Set();    // navbat keyingi threadga beriladi
         }
     }
 }
